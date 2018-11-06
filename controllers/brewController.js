74d1c014c5bba0a1822ab44e327a3a63be6afd98
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-06 16:28:09 -0600

    added index route

--- controllers/brewController.js
+++ controllers/brewController.js
@@ -4,5 +4,22 @@ const router = express.Router();
 const Brewery = require('../models/brews');
 
 
+router.get('/', async (req, res) => {
+
+  try {
+    const allBrews = await Brewery.find();
+
+    res.json({
+        status: 200,
+        data: allBrews
+      });
+
+  } catch (err) {
+    res.send(err)
+  }
+});
+
+
+
 
 module.exports = router;
